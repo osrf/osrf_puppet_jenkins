8ad38fb03f952deadd607deb68421864a8fc96c8
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-09-23 01:18:27 +0200

    Jenkins slave should be downloaded and installed

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,11 +1,49 @@
 class packages {
 
  package { "wget"            : ensure => installed }
- package { "jenkins-slave"   : ensure => installed }
  package { "mercurial"       : ensure => installed }
  package { "ntp"             : ensure => installed }
  package { "lightdm"         : ensure => installed }
  package { "docker.io"       : ensure => installed }
  package { "squid-deb-proxy" : ensure => installed }
+ package { "default-jre-headless" : ensure => installed }
 
+ define url-package (
+  $url,
+  $provider,
+  $package = undef) 
+ {
+
+  if $package {
+    $package_real = $package
+  } else {
+    $package_real = $title
+  }
+
+  $package_path = "/tmp/${package_real}"
+
+  exec {'download':
+    command => "/usr/bin/wget -O ${package_path} ${url}"
+  }
+
+  package {'install':
+    ensure   => installed,
+    name     => "${package}",
+    provider => 'dpkg',
+    source   => "${package_path}",
+  }
+
+  file {'cleanup':
+    ensure => absent,
+    path   => "${package_path}",
+  }
+
+  Exec['download'] -> Package['install'] -> File['cleanup']
+
+ }
+
+ url-package { "jenkins-slave" :
+    provider  => dpkg,
+    url       => http://mirrors.kernel.org/ubuntu/pool/universe/j/jenkins/jenkins-slave_1.509.2+dfsg-2_all.deb
+ }
 }
