6af28eecc9eabb2fbc8f7f95b5fa9f0186ab8b00
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-04-29 02:32:11 +0200

    Define function, delete_lines

--- manifests/files.pp
+++ manifests/files.pp
@@ -1,6 +1,12 @@
 $confdir_setting = $settings::confdir
 class files {
 
+  define delete_lines($file, $pattern) {
+    exec { "/bin/sed -i -r -e '/$pattern/d' $file":
+        onlyif => "/bin/grep -E '$pattern' '$file'",
+    }
+  }
+
   file_line { "/etc/sudoers":
     ensure => present,
     line   => 'jenkins ALL=(ALL) NOPASSWD:ALL',
